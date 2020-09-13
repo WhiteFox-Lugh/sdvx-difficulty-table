c312bef58f7e8d39d3b61c1041e517d921cd07ad
WhiteFox-Lugh <38004439+WhiteFox-Lugh@users.noreply.github.com>
2020-09-13 18:23:15 +0900

    fix : migrate

--- db/migrate/20200823232558_create_users.rb
+++ db/migrate/20200823232558_create_users.rb
@@ -1,6 +1,6 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
   def change
-    create_table :users, options: 'CHARSET=utf8mb4' do |t|
+    create_table :users do |t|
       t.string :uid
       t.string :nickname
       t.string :name
